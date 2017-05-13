cc2b55442c0594e8267c5587329c3355807b2dcf
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 13:06:26 -0400

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate void MyDel(int value);     // Declare delegate TYPE.
+
+        static void Main(string[] args)
+        {
+           Program program = new Program();
+            
+            MyDel myDel;      // Declare delegate variable.
+            
+            Random rand = new Random();
+            int randomValue = rand.Next(99);
+
+            // Create a delegate object that contains either PrintLow or PrintHigh, and 
+            // assign the object to the delegate variable.
+            myDel = randomValue < 50
+                        ? new MyDel(program.PrintLow)
+                        : new MyDel(program.PrintHigh);
+
+            myDel(randomValue);       // Execute the delegate.
+
+        }
+
+        void PrintLow(int value)
+        {
+            Console.WriteLine("{0} - Low Value", value);
+            Console.Read();
+        }
+
+        void PrintHigh(int value)
+        {
+            Console.WriteLine("{0} - High Value", value);
+            Console.Read();
+
+        }
+
+    }
+}
